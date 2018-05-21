7557b2eb6f90fdb69e475ff893cb8b05528f986c
qianxuecheng27 <385411788@qq.com>
2018-05-21 15:34:47 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings():
+    #存储外星人入侵的所有设置的类
+    
+    def __init__(self):
+        """初始化游戏的设置"""
+        #screen setting
+        self.screen_width = 1200
+        self.screen_height = 675
+        self.bg_color = (230, 230, 230)
+
+        #ship setting
+        self.ship_speed_factor = 5
+
+        #bullet setting
+        self.bullet_speed_factor = 10
+        self.bullet_width = 3
+        self.bullet_height =15
+        self.bullet_color = 60, 60, 60
+        self.bullet_allowed = 3
---
+++ ship.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Ship():
+
+    def __init__(self, ai_settings, screen):
+        """初始化飞船并设置初始位置"""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        #加载飞船图像并获取其外形矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #将每一艘新的飞船都放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        #attribute center save float
+        self.center = float(self.rect.centerx)
+
+        #moving sign
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据moving sign来调整飞船的坐标"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left >0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        #pass value of self.center to self.centerx
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
